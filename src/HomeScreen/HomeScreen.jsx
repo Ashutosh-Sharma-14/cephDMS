383d6536ac6735eaabcabb31c837faba9f514fae
amang9833 <amang9833@gmail.com>
2024-06-11 17:16:52 +0530

    fix the homeScreen Components

--- src/HomeScreen/HomeScreen.jsx
+++ src/HomeScreen/HomeScreen.jsx
@@ -5,13 +5,12 @@ const HomeScreen = () => {
   <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
     <div class="mx-auto max-w-xl text-center">
       <h1 class="text-3xl font-extrabold sm:text-5xl">
-        Understand User Flow.
-        <strong class="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
+        Document Management 
+        <strong class="font-extrabold text-red-700 sm:block">System Using Ceph. </strong>
       </h1>
 
       <p class="mt-4 sm:text-xl/relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-        numquam ea!
+      A system that can stand by Your Traffic demand
       </p>
 
       <div class="mt-8 flex flex-wrap justify-center gap-4">
@@ -19,16 +18,17 @@ const HomeScreen = () => {
           class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
           href="/"
         >
-          Get Started
+          Documentations
         </a>
 
         <a
           class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
           href="/"
         >
-          Learn More
+          Learn More about System
         </a>
       </div>
+
     </div>
   </div>
 </section>
