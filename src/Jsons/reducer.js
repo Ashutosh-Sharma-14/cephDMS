c73e038a1466de499915861bef5caa35488af77b
amang9833 <amang9833@gmail.com>
2024-06-24 19:56:44 +0530

    add the download butn in object card

--- src/Jsons/reducer.js
+++ src/Jsons/reducer.js
@@ -1,13 +1,9 @@
+import actionRequired from './actionReducer.json'
+
 export const reducer = (state, action) => {
     switch (action.type) {
-      case 'INCREMENT':
+      case actionRequired.logout:
         return { ...state, count: state.count + 1 };
-      case 'DECREMENT':
-        return { ...state, count: state.count - 1 };
-      case 'SET_USER':
-        return { ...state, user: action.payload };
-    case 'm':
-        return {};
       default:
         return state;
     }
@@ -15,6 +11,6 @@ export const reducer = (state, action) => {
 
 
   export const initialState = {
-    count: 0,
-    user: null
+    login:null,
+    logout:null
   };
